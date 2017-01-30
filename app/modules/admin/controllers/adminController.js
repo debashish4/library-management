e90404842de059a5cd832c977653d236de48e55c
Debashish <debashish4@gmail.com>
2017-01-31 01:33:13 +0530

    error handling on browser history clear

--- app/modules/admin/controllers/adminController.js
+++ app/modules/admin/controllers/adminController.js
@@ -12,6 +12,11 @@
 
 	    $cookieStore.get('loggedInAdmin');
 	    var loggedInAdmin = $cookieStore.get('loggedInAdmin');
+
+	    if(!loggedInAdmin){
+	    	 $state.go('login');
+	    	 return;
+	    }
 	    vm.profileData = candidateDataService.fetchData(loggedInAdmin).then(function() {
 	        if (loggedInAdmin) {
 	            vm.userData = {};
@@ -19,7 +24,7 @@
 	            vm.userName = vm.userData.name;
 	            vm.userType = vm.userData.type;
 	        } else {
-	            $state.go('login');
+	           
 	        }
 	    });
 
